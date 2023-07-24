52ddf586ed2895b6bca23723180ef30a4183cd40
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2023-07-23 20:03:17 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install required Python packages using pip3
+pip3 install tk
+pip3 install yfinance
+pip3 install prettytable
+pip3 install TA-Lib
+pip3 install alpaca-trade-api
+pip3 install pytz
+
+echo "Installation completed."
