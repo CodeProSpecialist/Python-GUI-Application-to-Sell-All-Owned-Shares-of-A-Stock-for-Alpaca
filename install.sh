96e21862650a0947256ab4236ede924f7145b277
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2023-07-23 20:14:39 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,54 @@
 #!/bin/bash
 
+echo "Starting setup..."
+
+# Check if python3 is installed
+if ! command -v python3 &> /dev/null
+then
+    echo "Python3 could not be found. Attempting to install..."
+    sudo apt update
+    sudo apt install -y python3-all
+fi
+
+# Check if pip3 is installed
+if ! command -v pip3 &> /dev/null
+then
+    echo "pip3 could not be found. Attempting to install..."
+    sudo apt update
+    sudo apt install -y python3-pip
+fi
+
 # Install required Python packages using pip3
-pip3 install tk
-pip3 install yfinance
-pip3 install prettytable
-pip3 install TA-Lib
-pip3 install alpaca-trade-api
-pip3 install pytz
+sudo pip3 install tk yfinance prettytable TA-Lib alpaca-trade-api pytz 
+
+# Check if build-essential is installed
+if ! command -v gcc &> /dev/null
+then
+    echo "gcc could not be found. Attempting to install build-essential..."
+    sudo apt update
+    sudo apt install -y build-essential
+fi
+
+# Install TA-Lib
+echo "Installing TA-Lib..."
+cd /tmp
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -xvzf ta-lib-0.4.0-src.tar.gz
+cd ta-lib/
+./configure --prefix=/usr
+make
+sudo make install
+
+# Check if TA-Lib installed correctly
+if ! ldconfig -p | grep libta_lib.so &> /dev/null
+then
+    echo "TA-Lib could not be found. The installation failed..."
+    exit 1
+fi
+
+# Install TA-Lib python wrapper
+sudo pip3 install ta-lib
+
+echo "Setup completed successfully."
 
 echo "Installation completed."
